fcb37d7f6430f23ff9179f1d42a95c60964935cf
Birintan <birintancs@gmail.com>
2022-06-19 02:29:45 -0400

    FInish css style for calculator
    follows apple's calculator theme

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-function add(a, b){
-    return a+b;
-}
-
-function subtract(a, b){
-    return a-b;
-}
-
-function multiply(a, b){
-    return a*b;
-}
-
-function divide(a, b){
-    return a/b;
+function operate(operation, a, b){
+    if(operation=="add"){
+        return a+b;
+    }
+    else if(operation==="subtract"){
+        return a-b;
+    }
+    else if(operation==="multiply"){
+        return a*b;
+    }
+    else if(operation==="divide"){
+        return a/b;  
+    }
 }
